dc6e1911050662ee8e1892d4cb61f6e52ab05c20
zpei <zhengqing.pei@gmail.com>
2018-11-02 20:28:58 -0400

    changed index to able to render other two components

--- configs.js
+++ configs.js
@@ -1,13 +1,12 @@
 const localHost = `http://localhost:3001`;
 const cloudHost = `https://mysterious-earth-97891.herokuapp.com`;
 
-export const host = process.env.NODE_ENV === 'development'
-? localHost
-: cloudHost
+export const host =
+  process.env.NODE_ENV === 'development' ? localHost : cloudHost;
 
 export const services = [
   {
     name: 'booker',
-    url: 'http://18.219.227.74/'
+    url: 'https://18.219.227.74/'
   }
 ];
